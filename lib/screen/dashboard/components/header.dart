0670942d9ebd4688d63bc405f3506a07babdbea1
tadakun8 <tadakun8.lab@gmail.com>
2021-12-29 15:52:02 +0900

    feat: Add profile card

--- lib/screen/dashboard/components/header.dart
+++ lib/screen/dashboard/components/header.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_responsive_admin_panel_or_dashboard_clone/constants.dart';
 
 class Header extends StatelessWidget {
   const Header({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
+    return Row(children: const [
       // Searchfield()
       Placeholder(),
       // ProfileCard()
-      Placeholder()
+      ProfileCard()
     ]);
+  }
+}
+
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: Constants.defaultPadding),
+      padding: const EdgeInsets.symmetric(
+        horizontal: Constants.defaultPadding,
+        vertical: Constants.defaultPadding / 2,
+      ),
+      decoration: BoxDecoration(
+        color: Constants.secondaryColor,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Colors.white12),
+      ),
+      child: Row(
+        children: [
+          Image.asset(
+            "assets/images/profile_pic.png",
+            height: 38,
+          ),
+          const Padding(
+            padding:
+                EdgeInsets.symmetric(horizontal: Constants.defaultPadding / 2),
+            child: Text('Angelina Jolie'),
+          ),
+          const Icon(Icons.keyboard_arrow_down)
+        ],
+      ),
+    );
   }
 }
