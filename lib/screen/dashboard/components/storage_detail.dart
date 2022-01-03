934f6d5c69b36410c37e14f9c29e7e2ecf0e983f
tadakun8 <tadakun8.lab@gmail.com>
2022-01-03 20:00:11 +0900

    feat: Add storage info

--- lib/screen/dashboard/components/storage_detail.dart
+++ lib/screen/dashboard/components/storage_detail.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_responsive_admin_panel_or_dashboard_clone/constants.dart';
 import 'package:flutter_responsive_admin_panel_or_dashboard_clone/screen/dashboard/components/chart.dart';
+import 'package:flutter_responsive_admin_panel_or_dashboard_clone/screen/dashboard/components/storage_info_card.dart';
 
 class StorageDetail extends StatelessWidget {
   const StorageDetail({Key? key}) : super(key: key);
@@ -23,7 +24,31 @@ class StorageDetail extends StatelessWidget {
           SizedBox(
             height: Constants.defaultPadding,
           ),
-          Chart()
+          Chart(),
+          StorageInfoCard(
+            svgSrc: "assets/icons/Documents.svg",
+            title: "Documents Files",
+            amountOfFiles: "1.3GB",
+            numOfFiles: 1328,
+          ),
+          StorageInfoCard(
+            svgSrc: "assets/icons/media.svg",
+            title: "Media Files",
+            amountOfFiles: "15.3GB",
+            numOfFiles: 1328,
+          ),
+          StorageInfoCard(
+            svgSrc: "assets/icons/folder.svg",
+            title: "Other Files",
+            amountOfFiles: "1.3GB",
+            numOfFiles: 1328,
+          ),
+          StorageInfoCard(
+            svgSrc: "assets/icons/unknown.svg",
+            title: "Unknown",
+            amountOfFiles: "1.3GB",
+            numOfFiles: 140,
+          ),
         ],
       ),
     );
